a0eb9b2d709e523f62a99dc3e05eb8bc661f4a34
Dmoore8122 <173196877+Dmoore8122@users.noreply.github.com>
2025-02-06 14:07:44 -0500

    I changed the eventlisteners for the buttons, so when pressed the get the compSelection and humSelection and call playRound. I also changed the else if statements so results and choice are displayed on window.

--- rps.js
+++ rps.js
@@ -1,10 +1,16 @@
 
+const rockB = document.querySelector('.rock-button');
+const paperB = document.querySelector('.paper-button');
+const scissorsB = document.querySelector('.scissors-button');
+const resultsDiv = document.querySelector('.results');
+const decisionDiv = document.querySelector('.decision')
 
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3)
     
     if (choice == 0) {
         return "rock"
+        
     }
     else if (choice == 1) {
         return "paper"
@@ -15,42 +21,59 @@ function getComputerChoice() {
 } 
 
 
-    
-
-function getHumanChoice() {
-    /*const playerSayWah = window.prompt('Rock, Paper, or Scissors?');
-    return playerSayWah;*/
-}
-
 
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        const humanSelection = getHumanChoice().toLowerCase();
+    rockB.addEventListener('click', () => {
         const computerSelection = getComputerChoice();
+        const humanSelection = "rock";
+        playRound(humanSelection, computerSelection);
+    });
+    paperB.addEventListener('click', () => {
+        const computerSelection = getComputerChoice();
+        const humanSelection = 'paper';
+        playRound(humanSelection, computerSelection)
+    });
+    scissorsB.addEventListener('click', () => {
+        const computerSelection = getComputerChoice();
+        const humanSelection = 'scissors';
+        playRound(humanSelection, computerSelection);
+    });
 
-        console.log(humanSelection);
-        console.log(computerSelection);
+
+    function playRound(humanSelection, computerSelection) {
+        const p1 = document.createElement('p');
+        p1.innerText = humanSelection;
+        decisionDiv.appendChild(p1);
+        const p2 = document.createElement('p');
+        p2.innerText = computerSelection;
+        decisionDiv.appendChild(p2);
 
         if (humanSelection == "rock" && computerSelection == "paper" ||
          humanSelection == "paper" && computerSelection == "scissors" ||
           humanSelection == "scissors" && computerSelection == "rock") {
 
             computerScore++;
-            console.log("Computer Wins!!" + humanScore + "-" + computerScore + ".");
+            const p = document.createElement('p');
+            p.innerText = "Computer Wins!!" + humanScore + "-" + computerScore + ".";
+            resultsDiv.appendChild(p);
 
         } else if (humanSelection === "paper" && computerSelection === "rock" ||
          humanSelection == "scissors" && computerSelection == "paper" ||
           humanSelection == "rock" && computerSelection == "scissors") {
 
         humanScore++;
-        console.log("You Win!!" + humanScore + "-" + computerScore + ".");
+        const p = document.createElement('p');
+        p.innerText = "You Win!!" + humanScore + "-" + computerScore + ".";
+        resultsDiv.appendChild(p);
 
         } else {
-        console.log("Its a Tie!!")
+            const p = document.createElement('p');
+            p.innerText = "Its a Tie!!";
+            resultsDiv.appendChild(p);
         }
     }
 
@@ -65,11 +88,12 @@ function playGame() {
     }
 
    
-    /*playRound()
+   /* playRound()
     playRound()
     playRound()
     playRound()
     playRound()*/
+    
 
     
     
